f3bcd00e89a8c4cfe2f5f14e7ef7a0549af18720
gothstar <vla9659&@gmail.com>
2019-04-09 01:45:01 +0300

    Swift 4.2 update Fixed dependencies for CGTK (no more downloading needed) **Tested on Ubuntu 18.04

---
+++ Libraries/CGtk-Linux/header.h
@@ -0,0 +1,8 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <termios.h>
+
+
+//
+//#include <gtk/gtkx.h>
+//#include <gtk/gtk-a11y.h>
--- Package.swift
+++ Package.swift
@@ -1,17 +1,47 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 #if os(OSX)
-    let gtkUrl = "https://github.com/TomasLinhart/CGtk-OSX"
+let gtkUrl = "-Mac"
 #elseif os(Linux)
-    let gtkUrl = "https://github.com/TomasLinhart/CGtk-Linux"
+let gtkUrl = "-Linux"
 #else
-    fatalError("Unsupported platform.")
+fatalError("Unsupported platform.")
 #endif
 
 let package = Package(
-  name:  "SwiftGtk",
-  dependencies: [
-    .Package(url: gtkUrl, majorVersion: 1, minor: 3)
-  ],
-  exclude: ["Sources/Ancillary Files"]
+        name: "SwiftGtk",
+        products: [
+            .library(name: "SwiftGtk",
+                    targets: ["SwiftGtk"]
+            )
+            ,
+//            .executable(name: "SwiftGtkDemo",
+//                    targets: ["SwiftGtkDemo"])
+        ],
+        targets: [
+            .systemLibrary(
+                    name: "CGtk",
+                    path: "Libraries/CGtk\(gtkUrl)", //gtkPath,
+                    pkgConfig: "gtk+-3.0",
+                    providers: [.apt(["gtk3.0-dev"]),
+                                .brew(["pkg-config", "gtk+3"])]),
+
+            .target(
+                    name: "SwiftGtk",
+                    dependencies: ["CGtk"],
+                    path: "Sources"
+            )
+//                    ,
+//            .target(
+//                    name: "SwiftGtkDemo",
+//                    dependencies: ["SwiftGtk"],
+//                    path: "SwiftGtkDemo"
+//            )//,
+            //   .testTarget(
+            //          name: "SwiftGtkDemoTests",
+            //         dependencies: ["SwiftGtkDemo"]
+            //    )
+        ]
+
 )
